5fd4fd0ef70ee3de95f4a722647327769bd4ce76
XinyiLu <xinyi.lu@thoughtworks.com>
2021-09-14 15:10:05 +0800

    Refactor

--- src/main/java/com/chunmiao/jsontotxt/JsonToTxtApplication.java
+++ src/main/java/com/chunmiao/jsontotxt/JsonToTxtApplication.java
@@ -17,92 +17,31 @@ public class JsonToTxtApplication {
         FileInputStream in = new FileInputStream("1.txt");
         JsonNode jsonNode = new ObjectMapper().readTree(in);
 
-        /**
-         * 取所有数据并存到HashMap中
-         */
-        String api;
-        HashMap<String, List<Root>> hm = new HashMap<>();
-        JsonNode node = jsonNode.findValue("paths");
-        Iterator<String> stringIterator = node.fieldNames();
-        while (stringIterator.hasNext()) {
-            JsonNode tags = node.findValue((api = stringIterator.next())); //api
-            Iterator<String> methodsname = tags.fieldNames();
-            while (methodsname.hasNext()) {
-                String method = methodsname.next(); //方法
-                JsonNode methods = tags.findValue(method);
+        List<Root> rootList = new ArrayList<>();
+        Iterator<Map.Entry<String, JsonNode>> node = jsonNode.findValue("paths").fields();
+        while (node.hasNext()){
+            Map.Entry<String, JsonNode> jsonNodeEntry = node.next();
+            String api = jsonNodeEntry.getKey();
+            JsonNode nodeValue = jsonNodeEntry.getValue();
+            Iterator<String> methodsName = nodeValue.fieldNames();
+            while (methodsName.hasNext()) {
+                String method = methodsName.next();
+                JsonNode methods = nodeValue.findValue(method);
                 String name = methods.findValue("tags").get(0).asText();
                 String description = methods.findValue("description").asText();
-
-                Root root = new Root(name, method, api,description);  //当前查询到的一个接口数据
-                //放到hashmap里管理
-                if (hm.containsKey(root.getName())) {
-                    List<Root> roots = hm.get(root.getName());
-                    roots.add(root);
-                    hm.put(root.getName(), roots);
-                } else {
-                    ArrayList<Root> roots = new ArrayList<>();
-                    roots.add(root);
-                    hm.put(root.getName(), roots);
-                }
-
+                Root root = new Root(name, method, api, description);
+                rootList.add(root);
             }
-
         }
-
-        /**
-         * 获得name的顺序，并按顺序写入csv
-         */
         File file = new File("result.csv");
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(file), "GBK"));    //excel不能读取utf-8编码的csv文件
-
-        Iterator<JsonNode> names = jsonNode.findValue("tags").iterator();
-        while (names.hasNext()) {
-            String name = names.next().findValue("name").asText();
-            Iterator<Root> iterator1 = hm.get(name).iterator();
-            bufferedWriter.write(name + ",");
-            Boolean isFirst = true;
-            while (iterator1.hasNext()) {
-                if (!isFirst) {
-                    bufferedWriter.write(",");
-                } else {
-                    isFirst = false;
-                }
-                Root next = iterator1.next();
-                bufferedWriter.write(next.getMethod() + "," +
-                        next.getApi() + "," + next.getDescription());
-                bufferedWriter.newLine();
-            }
-
+                new FileOutputStream(file), "GBK"));
+        for (Root root : rootList) {
+            bufferedWriter.write(root.getMethod().toUpperCase(Locale.ROOT) + " " +
+                    root.getApi());
+            bufferedWriter.newLine();
         }
         bufferedWriter.close();
-
-//
-//        DrugPricLmtRemoteService
-//        DrugPubonlnService
-//        McsPubonlnService
-//        MedinsPrucPlanService
-//        MedinsPurcOrdInService
-//        bufferedWriter.write(name + ","
-//                        + method +","
-//                        + api);
-//                bufferedWriter.newLine();
-//        bufferedWriter.close();
-        Runtime.getRuntime().exec("cmd /c start F:/Project/JsonSoup/result.csv");
-        System.out.println("done");
-
-
+        System.out.println("Convert Done");
     }
-
-
-    public static Root jsontoentity(FileInputStream in) throws IOException {
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        Root root = objectMapper.readValue(in, Root.class);
-
-        return root;
-
-
-    }
-
 }
